c7faeb02bef2643e2480a169a21f16f045124fc6
zzmx-sudo <972167377@qq.com>
2023-10-10 00:00:16 +0800

    完善操作手机获取验证码逻辑

--- android/model.py
+++ android/model.py
@@ -12,12 +12,35 @@ class MangoTVModel:
             raise SystemError(f"环境校验不通过, 初始化模块失败, 环境校验信息: {err_init_msg}")
 
         self._driver = None
+        self._window_width = 0
+        self._window_height = 0
         self._setup()
 
     def generate_sms_code(self, phone_number):
         """
         填入手机号并点击获取验证码
         """
+        self._driver.find_element_by_id("tvNickname").click()
+        elements = self._driver.find_elements_by_id("tvText")
+        if elements:
+            for element in elements:
+                if element.text == "切换账号":
+                    element.click()
+                    break
+
+        phone_clear = self._driver.find_element_by_id("ivClear")
+        if phone_clear: phone_clear.click()
+
+        elements = self._driver.find_elements_by_id("etContent")
+        if elements:
+            for element in elements:
+                if element.text == "请输入手机号":
+                    element.click()
+                    element.send_keys(phone_number)
+                    break
+        self._driver.find_element_by_id("checkBox").click()
+        self._driver.find_element_by_id("tvCheck").click()
+        self._driver.find_element_by_id("ivRight").click()
 
     def login_and_fillIn_redeemCode(self, phone_number, smsCode, redeemCode):
         """
@@ -59,8 +82,8 @@ class MangoTVModel:
             'platformName': 'Android',
             'platformVersion': self._android_version,
             'deviceName': 'xxx',
-            'appPackage': 'tv.danmaku.bili',
-            'appActivity': '.ui.splash.SplashActivity',
+            'appPackage': 'com.hunantv.imgo.activity',
+            'appActivity': '.MainActivity',
             'unicodeKeyboard': True,
             'resetKeyboard': True,
             'noReset': True,
@@ -72,10 +95,49 @@ class MangoTVModel:
         except Exception as e:
             raise SystemError(f"尝试连接手机异常, 原始错误: {e}")
 
-        # TODO: 点击个人信息页面  若登录退出
-        # 若有跳过按钮则点击
+        self._window_width = self._driver.get_window_size()["width"]
+        self._window_height = self._driver.get_window_size()["height"]
+        # 配置缺省等待正常打开APP的时间
+        self._driver.implicitly_wait(10)
+
+        # 进入APP后确认是退出登录状态
+        self._check_back_profile()
 
     def _check_back_profile(self):
         """
-        校验APP已经退出账号并在个人页面
+        校验APP已经退出账号并在我的页面, 若不是进入该状态
         """
+        bottom_menus = self._driver.find_elements_by_id("viewTabPlaceholder")
+        if not bottom_menus:
+            return False
+
+        # 进入我的页面
+        bottom_menus[-1].click()
+
+        # 获取用户名, 若用户名不为立即登录则表示已经登录, 需退出登录
+        username = self._driver.find_element_by_id("tvNickname")
+        if username.text != "立即登录":
+            self._logout()
+
+    def _logout(self):
+        """
+        退出用户登录
+        :return: bool -> 是否成功退出登录
+        """
+        setting = self._driver.find_element_by_id("setting")
+        if not setting:
+            return False
+        setting.click()
+
+        while True:
+            self._driver.swipe(
+                self._window_width*0.5, self._window_height*0.9,
+                self._window_width*0.5, self._window_height*0.1,
+                1000
+            )
+            elements = self._driver.find_elements_by_id("tvTitle")
+            if elements and elements[-1].text == "退出登录":
+                break
+
+        self._driver.find_elements_by_id("tvTitle")[-1].click()
+        return True
