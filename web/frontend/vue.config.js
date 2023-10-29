572576baf045c5a64f8496d60fdac4a89919e004
大宝天天见丶 <mr.cheng@MrchengdeMacBook-Pro.local>
2023-10-29 20:33:26 +0800

    配置前端打包输出路径

--- web/frontend/vue.config.js
+++ web/frontend/vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  assetsDir: "static",
   devServer: {
     port: "8080",
     open: true,
